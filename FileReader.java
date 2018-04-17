4d4d8a0f13b977483f5f348fa7281193d1339c63
mattchamp1997 <c16308053@mydit.ie>
2018-04-17 02:14:31 +0100

    Commit 3
    Very little work done. Few quick lines of code to get things started.

--- FileReader.java
+++ FileReader.java
@@ -1,6 +1,15 @@
 package assignment;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+
+import javax.swing.*;
+import java.util.*;
+
 public class FileReader 
-{
+{	
+	File swearPosts = new File("C:\\Users\\Matthew Champion\\Desktop\\abusivetweets.txt");
+	Scanner fileScan1 = new Scanner(swearPosts);
 
 }
